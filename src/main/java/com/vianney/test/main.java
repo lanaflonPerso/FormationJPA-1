3b5bbaf4fab8ee40e460c8e4dc1b1bb9d07cc4ab
vianneyba <vianneyba@free.fr>
2018-09-21 09:44:37 +0200

    autre

--- src/main/java/com/vianney/test/main.java
+++ src/main/java/com/vianney/test/main.java
@@ -1,16 +1,11 @@
 package com.vianney.test;
 
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
-import java.util.Set;
 
 import javax.persistence.EntityManager;
 
-import com.vianney.Session;
 import com.vianney.Stagiaire;
 import com.vianney.dao.Dao;
-import com.vianney.dao.SessionDao;
 import com.vianney.dao.StagiaireDao;
 import com.vianney.fixture.Fixture;
 import com.vianney.tools.PersistenceUnitFactory;
